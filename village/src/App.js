3d0ac4eb54d251c5cec6e871b914a40cc4cdd7a8
Derek Jones <derek@derekjones.io>
2019-02-09 13:45:00 -0500

    Fix last commit - addSmurf was in the wrong place

--- village/src/App.js
+++ village/src/App.js
@@ -25,20 +25,6 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
-  addSmurf = () => {
-    axios
-      .post('http://localhost:3333/smurfs')
-      .then(res => this.setState({ smurfs: res.data }))
-      .catch(err => console.log(err));
-  }
-
-  deleteSmurf = () => {
-    axios
-      .delete('http://localhost:3333/smurfs/${id}')
-      .then(res => this.setState({ smurfs: res.data }))
-      .catch(err => console.log(err));
-  }
-
   render() {
     return (
       <div className="App">
